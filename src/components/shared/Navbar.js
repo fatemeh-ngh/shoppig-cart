791057cf8e576fd1670c708ca29bea02df95af71
Fatemeh Negahdari <ftm.ngh.2001@gmail.com>
2022-07-08 15:11:16 +0430

    add the styles for navbar and product cart

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -7,15 +7,18 @@ import { cartContext } from '../../context/CartContextProvider';
 //icons
 import shopIcon from '../../assets/icons/shop.svg';
 
+//style
+import styles from './Navbar.module.css';
+
 const Navbar = () => {
     
     const {state} = useContext(cartContext)
 
     return (
-        <div>
-            <div>
-                <Link to="/products">Products</Link>
-                <div>
+        <div className={styles.mainContainer}>
+            <div className={styles.container}>
+                <Link className={styles.productLink} to="/products">Products</Link>
+                <div className={styles.iconContainer}>
                     <Link to="/cart"><img src={shopIcon} alt="shop icon" /></Link>
                     <span>{state.itemsCounter}</span>
                 </div>
--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -10,23 +10,26 @@ import { cartContext } from '../../context/CartContextProvider';
 //icons 
 import trashicon from '../../assets/icons/trash.svg'
 
+//styles
+import styles from './Product.module.css'
+
 const Product = ({productData}) => {
 
     const {state, dispatch} = useContext(cartContext);
 
     return (
-        <div>
-            <img src={productData.image} alt="product" style={{width: "200px"}}/>
+        <div className={styles.container}>
+            <img  className={styles.cardImage} src={productData.image} alt="product" style={{width: "200px"}}/>
             <h3>{shorter(productData.title)}</h3>
             <p>{`${productData.price}$`}</p>
-            <div>
+            <div  className={styles.linkContainer}>
                 <Link to={`/products/${productData.id}`}>detalis</Link>
-                <div>
-                    {quantityCount(state, productData.id) > 1 && <button onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button>}
-                    {quantityCount(state, productData.id) === 1 && <button onClick={() => dispatch({type: "REMOVE-ITEM", payload: productData})}><img src={trashicon} alt='trash icon' style={{width:'20px'}}/></button>}
+                <div className={styles.buttonContainer}>
+                    {quantityCount(state, productData.id) > 1 && <button className={styles.smallButton} onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button>}
+                    {quantityCount(state, productData.id) === 1 && <button className={styles.smallButton} onClick={() => dispatch({type: "REMOVE-ITEM", payload: productData})}><img src={trashicon} alt='trash icon' style={{width:'20px'}}/></button>}
                     {
                         isInCart(state, productData.id) ?
-                        <button onClick={() => dispatch({type: "INCREASE", payload: productData})}>+</button> :
+                        <button className={styles.smallButton} onClick={() => dispatch({type: "INCREASE", payload: productData})}>+</button> :
                         <button onClick={() => dispatch({type: "ADD_ITEM", payload: productData})}>Add to Cart</button>
                     }
                 </div>
